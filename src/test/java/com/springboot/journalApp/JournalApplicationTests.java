103d3ed382b78bbf153b700baf398b763eafd2a3
vishwarup-kashyap <vishwarup.kashyap2003@gmail.com>
2025-08-03 00:15:39 +0530

    disabling test

--- src/test/java/com/springboot/journalApp/JournalApplicationTests.java
+++ src/test/java/com/springboot/journalApp/JournalApplicationTests.java
@@ -1,13 +1,9 @@
 package com.springboot.journalApp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JournalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
